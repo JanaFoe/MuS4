c68340503111408a54d9dc38cf284c6279f07c84
TanjaHunsinger <tanja.hunsinger@student.uni-tuebingen.de>
2017-11-14 14:49:18 +0100

    add zweiter test aus der vorlesung funktioniert

--- DES/src/study/AutocorrelationTest.java
+++ DES/src/study/AutocorrelationTest.java
@@ -11,7 +11,7 @@ public class AutocorrelationTest {
 
     public static void testAutocorrelation() {
     	
-    	DiscreteAutocorrelationCounter test = new DiscreteAutocorrelationCounter("test", 5);
+    	DiscreteAutocorrelationCounter test = new DiscreteAutocorrelationCounter("X = 0,1,....9", 9);
     	test.reset();
   	
     	for (double i = 0; i< 10; i++) {
@@ -19,5 +19,17 @@ public class AutocorrelationTest {
 		}
     	
     	System.out.println(test.report());
+    	
+    	
+    	DiscreteAutocorrelationCounter test1 = new DiscreteAutocorrelationCounter("X = 0,1,1,2", 3);
+    	test1.reset();
+  	
+    	for (double i = 0; i < 4; i++) {
+    		if(i == 0)	test1.count(0);
+    		else if(i == 1 || i == 2) test1.count(1);
+    		else if(i == 3)  test1.count(2);
+ 		}
+    	
+    	System.out.println(test1.report());
     }
 }
